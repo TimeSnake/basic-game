98174ab15d65ac2a638563b7d7e2534a8050bff2
funzter <funzter@mail.timesnake.de>
2022-03-01 16:14:39 +0100

    added discord channel names

--- src/main/java/de/timesnake/basic/game/util/GameServer.java
+++ src/main/java/de/timesnake/basic/game/util/GameServer.java
@@ -4,6 +4,9 @@ import de.timesnake.basic.bukkit.util.Server;
 
 public class GameServer extends Server {
 
+    public static final String DISCORD_SPECTATOR = "Spectator";
+    public static final String DISCORD_LOUNGE = "Lounge";
+
     private static final GameServerManager server = GameServerManager.getInstance();
 
     public static Game getGame() {
