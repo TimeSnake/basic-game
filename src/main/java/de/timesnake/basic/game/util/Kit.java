ad65dd48981e90266aee97809a3171f07187bd1b
funzter <cavehive@protonmail.com>
2022-10-08 11:44:44 +0200

    added license and copyright

--- src/main/java/de/timesnake/basic/game/util/Kit.java
+++ src/main/java/de/timesnake/basic/game/util/Kit.java
@@ -1,3 +1,21 @@
+/*
+ * basic-game.main
+ * Copyright (C) 2022 timesnake
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.timesnake.basic.game.util;
 
 import de.timesnake.basic.bukkit.util.Server;
@@ -16,8 +34,8 @@ public class Kit {
 
     private final Integer id;
     private final String name;
-    private Material material;
     private final List<String> description = new ArrayList<>();
+    private Material material;
 
     public Kit(Integer id, String name, Material material, List<String> description) {
         this.id = id;
