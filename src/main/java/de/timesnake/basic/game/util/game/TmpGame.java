033c3a8eb4b4c4ae378f69198e269fa79917e949
funzter <cavehive@protonmail.com>
2023-01-11 18:21:07 +0100

    cleaned up

--- src/main/java/de/timesnake/basic/game/util/game/TmpGame.java
+++ src/main/java/de/timesnake/basic/game/util/game/TmpGame.java
@@ -13,15 +13,17 @@ import de.timesnake.database.util.object.Type;
 import de.timesnake.library.game.TmpGameInfo;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.TreeSet;
 import org.bukkit.inventory.ItemStack;
 
 public class TmpGame extends Game<TmpGameInfo> {
 
     protected final java.util.Map<String, Team> teamsByName = new HashMap<>();
-    protected final LinkedHashMap<Integer, Team> teamsSortedByRank = new LinkedHashMap<>();
+    protected final TreeSet<Team> teamsSortedByRank = new TreeSet<>(
+            Comparator.comparingInt(Team::getRank));
 
     public TmpGame(DbTmpGame database, boolean loadWorlds) {
         super(database, new TmpGameInfo(database.getInfo()), loadWorlds);
@@ -31,17 +33,16 @@ public class TmpGame extends Game<TmpGameInfo> {
             try {
                 team = this.loadTeam(dbTeam);
             } catch (UnsupportedGroupRankException e) {
-                Server.printWarning(Plugin.BUKKIT, de.timesnake.basic.bukkit.util.chat.ChatColor.WARNING + e.getMessage());
+                Server.printWarning(Plugin.BUKKIT,
+                        de.timesnake.basic.bukkit.util.chat.ChatColor.WARNING + e.getMessage());
                 continue;
             }
 
             if (team != null) {
                 this.teamsByName.put(team.getName(), team);
-                this.teamsSortedByRank.put(team.getRank(), team);
+                this.teamsSortedByRank.add(team);
             }
         }
-        this.teamsSortedByRank.entrySet().stream().sorted(java.util.Map.Entry.comparingByKey()).forEach(java.util.Map.Entry::getKey);
-
     }
 
     public boolean hasTeam(String name) {
@@ -60,33 +61,34 @@ public class TmpGame extends Game<TmpGameInfo> {
         return teamsByName;
     }
 
-    public LinkedHashMap<Integer, ? extends Team> getTeamsSortedByRank() {
+    public TreeSet<Team> getTeamsSortedByRank() {
         return teamsSortedByRank;
     }
 
-    public LinkedHashMap<Integer, ? extends Team> getTeamsSortedByRank(int amount) {
-        LinkedHashMap<Integer, Team> teams = new LinkedHashMap<>();
-        for (java.util.Map.Entry<Integer, Team> entry : teamsSortedByRank.entrySet()) {
-            if (amount <= 0) {
-                break;
-            }
-            teams.put(entry.getKey(), entry.getValue());
-            amount--;
-        }
-        teams.entrySet().stream().sorted(java.util.Map.Entry.comparingByKey()).forEach(java.util.Map.Entry::getKey);
-        return teams;
+    public List<Team> getTeamsSortedByRank(int amount) {
+        return teamsSortedByRank.stream().limit(amount).toList();
     }
 
 
-    public Integer getAutoStartPlayerNumber() {return getInfo().getAutoStartPlayerNumber();}
+    public Integer getAutoStartPlayerNumber() {
+        return getInfo().getAutoStartPlayerNumber();
+    }
 
-    public Integer getMinPlayerNumber() {return getInfo().getMinPlayerNumber();}
+    public Integer getMinPlayerNumber() {
+        return getInfo().getMinPlayerNumber();
+    }
 
-    public List<Integer> getTeamSizes() {return getInfo().getTeamSizes();}
+    public List<Integer> getTeamSizes() {
+        return getInfo().getTeamSizes();
+    }
 
-    public Type.Availability getTeamMerge() {return getInfo().getTeamMerge();}
+    public Type.Availability getTeamMerge() {
+        return getInfo().getTeamMerge();
+    }
 
-    public boolean isEqualTimeSizeRequired() {return getInfo().isEqualTimeSizeRequired();}
+    public boolean isEqualTimeSizeRequired() {
+        return getInfo().isEqualTimeSizeRequired();
+    }
 
     public boolean showSelectedKits() {
         return getInfo().showSelectedKits();
@@ -96,27 +98,51 @@ public class TmpGame extends Game<TmpGameInfo> {
         return getInfo().hideTeams();
     }
 
-    public Type.Discord getDiscordType() {return getInfo().getDiscordType();}
+    public Type.Discord getDiscordType() {
+        return getInfo().getDiscordType();
+    }
 
-    public List<String> getDescription() {return getInfo().getDescription();}
+    public List<String> getDescription() {
+        return getInfo().getDescription();
+    }
 
-    public String getName() {return getInfo().getName();}
+    public String getName() {
+        return getInfo().getName();
+    }
 
-    public String getDisplayName() {return getInfo().getDisplayName();}
+    public String getDisplayName() {
+        return getInfo().getDisplayName();
+    }
 
-    public Integer getMaxPlayers() {return getInfo().getMaxPlayers();}
+    public Integer getMaxPlayers() {
+        return getInfo().getMaxPlayers();
+    }
 
-    public String getHeadLine() {return getInfo().getHeadLine();}
+    public String getHeadLine() {
+        return getInfo().getHeadLine();
+    }
 
-    public ItemStack getItem() {return getInfo().getItem();}
+    public ItemStack getItem() {
+        return getInfo().getItem();
+    }
 
-    public Integer getSlot() {return getInfo().getSlot();}
+    public Integer getSlot() {
+        return getInfo().getSlot();
+    }
 
-    public Type.Availability getMapAvailability() {return getInfo().getMapAvailability();}
+    public Type.Availability getMapAvailability() {
+        return getInfo().getMapAvailability();
+    }
 
-    public Type.Availability getKitAvailability() {return getInfo().getKitAvailability();}
+    public Type.Availability getKitAvailability() {
+        return getInfo().getKitAvailability();
+    }
 
-    public String getTexturePackLink() {return getInfo().getTexturePackLink();}
+    public String getTexturePackLink() {
+        return getInfo().getTexturePackLink();
+    }
 
-    public Boolean hasTexturePack() {return getInfo().hasTexturePack();}
+    public Boolean hasTexturePack() {
+        return getInfo().hasTexturePack();
+    }
 }
