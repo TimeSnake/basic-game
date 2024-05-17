5a95ba9dc829a015dd3e5f0fd34b2fe88542c289
funzter <cavehive@protonmail.com>
2024-05-17 21:41:12 +0200

    minor

--- src/main/java/de/timesnake/basic/game/util/user/Plugin.java
+++ src/main/java/de/timesnake/basic/game/util/user/Plugin.java
@@ -4,19 +4,12 @@
 
 package de.timesnake.basic.game.util.user;
 
-import de.timesnake.library.basic.util.LogHelper;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Plugin extends de.timesnake.basic.bukkit.util.chat.Plugin {
 
-  public static final Plugin LOUNGE = new Plugin("Lounge", "BLB",
-      LogHelper.getLogger("Lounge", Level.INFO));
-  public static final Plugin GAME = new Plugin("Game", "BLG",
-      LogHelper.getLogger("Game", Level.INFO));
+  public static final Plugin LOUNGE = new Plugin("Lounge", "BLB");
+  public static final Plugin GAME = new Plugin("Game", "BLG");
 
-  protected Plugin(String name, String code, Logger logger) {
+  protected Plugin(String name, String code) {
     super(name, code);
   }
 }
